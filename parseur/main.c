bf7aaa5be7bf120afa854b2befca798702af65b8
grimms1337 <sylvain.grimm@viacesi.fr>
2015-12-14 17:17:14 +0100

    Mise à jour parseur

--- parseur/main.c
+++ parseur/main.c
@@ -6,6 +6,16 @@
 #include <unistd.h>
 #include <errno.h>
 
+ struct    command
+    {
+    char    *cmd;
+    char    **keyWords;
+    };
+
+struct command listecommandesnaturel;
+
+
+
 /* This is just a sample code, modify it to meet your need */
 int liredictionnaires()
 {
@@ -13,13 +23,29 @@ int liredictionnaires()
     struct dirent* in_file;
     FILE    *entry_file;
     char    cwd[1024];
+    char    dossierdictionnaires[50] = "dictionnaires/";
+    //char    chaine[100] = "";
+    int     count = 0;
+    //char    *premiereligne;
 
 
-   if (getcwd(cwd, sizeof(cwd)) != NULL)
-       fprintf(stdout, "Current working dir: %s\n", cwd);
-   else
-       perror("getcwd() error");
-       return 1;
+
+
+
+
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+
+    {
+    fprintf(stdout, "Current working dir: %s\n", cwd);
+    }
+
+    else
+
+   {
+    perror("getcwd() error");
+    return 1;
+    }
 
 
 
@@ -39,9 +65,14 @@ int liredictionnaires()
             continue;
         if (!strcmp (in_file->d_name, ".."))
             continue;
+
+
+
+
         /* Open directory entry file for common operation */
         /* TODO : change permissions to meet your need! */
-        entry_file = fopen(in_file->d_name, "r");
+        entry_file = fopen(strcat(dossierdictionnaires, in_file->d_name), "r");
+
         if (entry_file == NULL)
         {
             fprintf(stderr, "Error : Failed to open entry file - %s\n", strerror(errno));
@@ -49,11 +80,34 @@ int liredictionnaires()
             return 1;
         }
 
+        char line[256]; /* or other suitable maximum line size */
+        while (fgets(line, sizeof (line), entry_file) != NULL) /* read a line */
+        {
+            if (count == 1)
+            {
+            //use line or in a function return it
+            //in case of a return first close the file with "fclose(file);"
+
+                printf("%s", line);
+                count++;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+
+
+
+
 
 
 
 
         fclose(entry_file);
+
+        strcpy (dossierdictionnaires, "dictionnaires/");
     }
 
     /* Don't forget to close common file before leaving */
@@ -67,5 +121,5 @@ int main()
 
 liredictionnaires();
 
-return 1;
+return 0;
 }
